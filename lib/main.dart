2996833a820463f0af749a34e82bd711887eca35
eloketobe <sameloke@yahoo.com>
2021-07-03 12:06:29 +0100

    First commit for the dicee app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDieNumber = 1;
+  int rightDieNumber = 1;
+
+  void changeDie() {
+    setState(() {
+      leftDieNumber = Random().nextInt(6) + 1;
+      rightDieNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$leftDieNumber.png'),
+              onPressed: () {
+                changeDie();
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$rightDieNumber.png'),
+              onPressed: () {
+                changeDie();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
